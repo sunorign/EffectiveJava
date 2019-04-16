196e4b545c9fdbd39b278652be53286bf8a2a963
zexinChen <2455832363@qq.com>
2019-04-16 13:56:00 +0800

    second commit

--- src/main/java/注解技术/Basic.java
+++ src/main/java/注解技术/Basic.java
@@ -13,6 +13,7 @@ public class Basic {
 //                        .addField(FieldSpec.builder().build())               //属性
 //                        .addMethod(MethodSpec.constructorBuilder().build())              //方法
 //                        .build()).build();
+        //
     }
 }
 
