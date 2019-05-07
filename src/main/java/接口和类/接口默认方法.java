c0f9ed83b389654a777988840743c89d89bdf00a
zexinChen <2455832363@qq.com>
2019-05-07 17:21:36 +0800

    commit

--- src/main/java/接口和类/接口默认方法.java
+++ src/main/java/接口和类/接口默认方法.java
@@ -1,5 +1,8 @@
 package 接口和类;
 
+/**
+ * 接口默认方法：在接口中实现的方法，可以直接调用
+ */
 public class 接口默认方法 {
     public static void main(String[] args) {
         new InterfaceB() {}.foo(); // 打印：“InterfaceA foo”
@@ -21,16 +24,17 @@ interface InterfaceA {
     }
 }
 
+//继承InterfaceA 的默认方法
 interface InterfaceB extends InterfaceA {
 }
-
+//覆写InterfaceA的默认方法
 interface InterfaceC extends InterfaceA {
     @Override
     default void foo() {
         System.out.println("InterfaceC foo");
     }
 }
-
+//实现类中实现默认方法
 interface InterfaceD extends InterfaceA {
     @Override
     void foo();
