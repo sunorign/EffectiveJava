cac903f308150c3e47b80aec85a3c95df48d9b61
zexinChen <2455832363@qq.com>
2019-05-23 22:14:19 +0800

    commit

---
+++ src/main/java/枚举/消除非受检异常.java
@@ -0,0 +1,14 @@
+package 枚举;
+
+/**
+ * 要尽可能消除非受检异常，如果无法消除使用 @SuppressWarning("unchecked") 来屏蔽。@SuppressWarning 需谨慎使用。一旦使用必须加注释
+ */
+public class 消除非受检异常 {
+    /**
+     * java 异常 Throwable 分类：Error、Exception
+     *
+     * Exception 进一步划分：RuntimeException、Checked Exceptions
+     *
+     * RuntimeException 和 Error 是非受检异常（uncheckedException）
+     */
+}
